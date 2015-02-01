dbeac7fac659d5d5ae803a9480035f302d70ac6a
syves <7shakrahs@gmail.com>
2015-01-31 21:34:18 -0800

    awesome refactoring with partition and drop

--- recursive_sort.rb
+++ recursive_sort.rb
@@ -1,27 +1,19 @@
 #recursively sort an array
 
 def quick_sort(array)
-  pivot = array[0];
-  left = [];
-  right = [];
-  sorted = [];
  #until input is []
   if (array == [])
     []
   else
-    array[1..array.length].each do |num|
-      if (num <= pivot)
-        left << num
-      else
-        right << num
-      end
-    end
-    sorted << quick_sort(left) 
-    sorted << pivot 
-    sorted << quick_sort(right)
+    pivot = array[0]
+  
+    left, right = array.drop(1).partition {|n| n <= pivot }
+
+    quick_sort(left) + [pivot] + quick_sort(right)
   end
-  sorted.flatten
 end
 
-p quick_sort([9,8,7,6,5,4,3,2,1])
+p quick_sort([9,3,7,8,5,4,3,2,1])
+p quick_sort([])
+
 
