177ff926e8b5e0faa44f771ea1ebd1e4c04b5b09
Shakrah Yves <7shakrahs@gmail.com>
2018-04-24 20:10:33 +0200

    recursive getNextPrime works when num is 0

--- euler-10.clj
+++ euler-10.clj
@@ -1,22 +1,27 @@
 (ns "euler-problem-10")
 ;The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-;Find the sum of all the primes below two million.
+;1. Find the sum of all the primes below two million.
 
-;1. find a prime
+;A. find factors
+;B. find a prime
+;C. find next prime
 
 (defn factors? [num] (map (partial / num) (range 1 (inc num))))
-;(factors? 6) ;(6 3 2 3/2);[6,3,2]
-;(factors? 7) ;(7 7/2 7/3 7/4 7/5)
 
-(defn prime? [num] (empty? (remove #{num} (filter integer? (factors? num)))))
+(factors? 1) ;(1)
+(factors? 2) ;(2 1)
+(factors? 6) ;(6 3 2 3/2 6/5 1)
+(factors? 7) ;(7 7/2 7/3 7/4 7/5 7/6 1)
 
+(defn prime? [num] (if (= num 1)
+                       false
+                       (empty? (remove #{list num 1} (filter integer? (factors? num))))))
+(prime? 1) ;false
+(prime? 2) ;true
 (prime? 7) ;true
 (prime? 6) ;false
-(prime? 10000)
+(prime? 10000) ;false
 
-;while sum is <2 million find the next prime and add it to acc
-
-;if it is not prime  find the next prime else return the prime
 (defn getNextPrime
   [num]
   (loop [num num]
@@ -24,12 +29,14 @@
       (if (prime? maybe-prime)
           maybe-prime
           (recur maybe-prime)))))
-
-(getNextPrime 7);"false 8"
-(getNextPrime 6);"true 7"
+(getNextPrime 0) ;2
+(getNextPrime 7) ;11
+(getNextPrime 6) ;7
 
 (defn sumPrimes
   [max-num]
-  (let [acc 0
-        num 0]
-    if (< acc max-num) (+ acc (getNextPrime num))) (+ acc curremtPrime)) acc) )
+  (let [acc 0 num 0]
+    (while (< acc max-num)
+            (+ acc (getNextPrime num))
+            acc)))
+(sumPrimes 18)
