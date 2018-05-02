729fb7416b383a50d671151b2d6cfd6850b04363
Shakrah Yves <7shakrahs@gmail.com>
2018-05-02 13:34:51 +0200

    add notes

--- euler_prob_10.clj
+++ euler_prob_10.clj
@@ -1,38 +1,37 @@
-(ns "euler_problem_10")
-(require '[clojure.math.numeric-tower :as math])
+(ns euler_problem_10)
+  ;(:require [clojure.math.numeric-tower :as math]))
 
 ;The sum of the primes below n. is 2 + 3 + 5 + 7 = 17.
 ;1. Find the sum of primes below two million).
 
 ; Int -> List Numer ; could try using pre and post conditions
-(defn factors [num] (map (partial / num) (range 1 (inc num))))
+;(defn factors [num] (map (partial / num) (range 1 (inc num))))
+
+;TODO nested ifs are gross, why isnt there an else if?
+;TODO make prime check faster. Prime numbers have an irrational root, but there is no irrational type in Java that I can find. And one cannot simply square the root of n and compare it to N, because of precision loss on non primes as well.
 
 (defn faster-prime? [mp]
   (empty?
   (loop [maybe-prime mp maybe-factor 2 factors []]
     ;(println maybe-prime maybe-factor)
-    (if (= false (empty? factors))
+    (if (= false (empty? factors)) ;we found the first factor, escape loop
       factors
-      (if (< maybe-factor (dec maybe-prime))
+      (if (< maybe-factor (dec maybe-prime)) ;the prime itself would be a factor which we dont wan to capture
         (recur maybe-prime
                (inc maybe-factor)
                (if (= 0 (mod maybe-prime maybe-factor))
-                         (cons maybe-factor factors)
-                         factors))
-        factors)))))
+                         (cons maybe-factor factors);found
+                         factors));not found
+        factors))))) ;looked at all the factors, return results
 
-(lazy-factor 17) ; true
-(lazy-factor 18) ;false
-
-;cons n (lazy-seq (/ maybe-prime (inc maybe-factor)
+(faster-prime? 17) ; true
+(faster-prime? 18) ;false
 
 ;Int -> Boolean
 (defn prime? [num]
              (if (= num 1)
                  false
                  (empty? (remove #{num 1} (filter integer? (factors num))))))
-
-;TODO: could rewrite n-primes to create a lazy sequence
 
 (defn next-prime
   [num]
@@ -42,7 +41,6 @@
           maybe-prime
           (recur maybe-prime)))))
 
-;find all the primes below 2 million
 ;Int -> Int
 (defn sum-primes
   [num]
@@ -50,7 +48,9 @@
     (if (zero? num-primes)
         sum
         (recur (dec num-primes) (+ sum curr-prime) (next-prime curr-prime)))))
-  ;(reduce + (filter prime? (range 1 (inc num)))))
+
+;TODO how to make sumprimes lazy.. call next prime indef and then take of num-primes
+(defn natural-nums  [n] (lazy-seq (cons n (natural-nums (inc n)))))
 
 ; Int -> Int -> List Double
 ;why cant I access Math/pow when it is inside partial and map?
@@ -74,17 +74,8 @@
 (sum-primes 4)
 (sum-primes 5)
 (sum-primes 1000)
-(sum-primes 10000); around 2 minutes
-
-;maybe we should store each prime in a map. and exclude all mutiple of that prime from checking?
-;we would not need to factor it or filter it.
-;are most numbers divisable by 2, 3, 5, 7, 11, 13,
-;maybe get exponentsup to 10? are they the same as multiples?
-;..maybe we want filter before checking if something is prime.
-; if it in dictionary? it is a multiple of a prime we have already found
-;is it divisable by a larger even number than it is divied by 2,
-;[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17...54]
-;do prime numbers have a real root?
-
-
+;(sum-primes 10000); around 2 minutes
 ;(sum-primes 2000000)
+
+
+;do prime numbers have a real root?
