2c7c83fba8625d0d4d70b4c4990de04d28ef3264
Shakrah Yves <7shakrahs@gmail.com>
2018-05-02 23:18:30 +0200

    refactor faster-prime

--- euler_prob_10.clj
+++ euler_prob_10.clj
@@ -5,25 +5,41 @@
 ;1. Find the sum of primes below two million).
 
 ;TODO nested ifs are gross, why isnt there an else if?
-;TODO make prime check faster. Prime numbers have an irrational root, but there is no irrational type in Java that I can find. And one cannot simply square the root of n and compare it to N, because of precision loss on non primes as well.
+;TODO make prime check faster.
+;1. only search up to the square root of n,
+;only need to fnd the smaller member of the pair of the composite number.
+;2. store primes we find in a dictionary but why? if n is divisible by the prime then we dont have to check for any other factors.
+;ex n= 1764 , 42 * 42,  mod  42 7 = 0, 7 x 6 = 42,
+;fatcors of 42 = (42 21 14 7 6 3 2 1)
+;we would check 2,3,5,7 with new optamization
+;without would be 2, for non primes.
 
-(defn faster-prime? [mp]
-  (empty?
-  (loop [maybe-prime mp maybe-factor 2 factors []]
-    ;(println maybe-prime maybe-factor)
-    (if (= false (empty? factors)) ;we found the first factor, escape loop
-      factors
-      (if (< maybe-factor (dec maybe-prime)) ;the prime itself would be a factor which we dont wan to capture
-        (recur maybe-prime
-               (inc maybe-factor)
-               (if (= 0 (mod maybe-prime maybe-factor))
-                         (cons maybe-factor factors);found
-                         factors));not found
-        factors))))) ;looked at all the factors, return results
+;what about a prime?
+;541 sqrt 541 ~ 23.0,
+;huge work reduction here.
+;to find the next factor we would check
+;everytime we check primes that we have already found: 2,3,5,7,11,17,19
+;we check (2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21)
+;in this case we reduce the work by at 1/4,
+
+;Int -> Boolean
+;TODO add factors as an argument to loop
+;TODO find a way to reove factors
+
+(defn faster-prime? [maybe-prime]
+  (loop [maybe-factor 2 factors false]
+    (if (= true factors) ;we found the first factor, escape loop
+      false
+      (if (< maybe-factor (Math/floor (Math/sqrt maybe-prime)))
+          (recur  ; arg1
+               (inc maybe-factor) ;arg2
+               (= 0 (mod maybe-prime maybe-factor)))
+          true))))
 
 (faster-prime? 17) ; true
 (faster-prime? 18) ;false
 
+;finds a single prime number
 (defn next-prime
   [num]
   (loop [numbr num]
@@ -40,14 +56,18 @@
         sum
         (recur (dec num-primes) (+ sum curr-prime) (next-prime curr-prime)))))
 
+
 (defn n-primes-lazy [prime] (lazy-seq (cons prime (n-primes-lazy (next-prime prime)))))
+
 (take 5 (n-primes-lazy 2)) ;(2 3 5 7 11)
+(take 100 (n-primes-lazy 2))
 (reduce + (take 5 (n-primes-lazy 2)))
 
 ;this is not really a good case for laziness just we know exactly how many we want
 (defn lazy-sum-primes [num-primes] (reduce + (take num-primes (n-primes-lazy 2))))
 (lazy-sum-primes 1000)
-(lazy-sum-primes 10000) ;slightly over 2 minutes
+(lazy-sum-primes 10000) ;2 sec 489521294
+(lazy-sum-primes 2000000)
 
 ; Int -> Int -> List Double
 ;why cant I access Math/pow when it is inside partial and map?
@@ -60,5 +80,5 @@
 (sum-primes 4)
 (sum-primes 5)
 ;(sum-primes 1000)
-;(sum-primes 10000); around 2 minutes
+(sum-primes 10000)
 ;(sum-primes 2000000)
