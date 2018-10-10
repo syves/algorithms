a855b6116ecccbfb009bbfa641c9f51519ad5d21
Shakrah Yves <7shakrahs@gmail.com>
2018-10-10 15:56:42 +0200

    pairs passes tests

--- divisibleSumPair.scala
+++ divisibleSumPair.scala
@@ -1,17 +1,20 @@
 object Solution {
   def divisibleSumPairs(n: Int, k: Int, ar: Array[Int]): Int = {
     var count = 0
-    for (i <- ar) {
-      for (j <- ar) {
-        if (i < j && (i + j)% k == 0) count += 1
+    for (i <- 0 until n) {
+      for (j <- i+1 until n) {
+        if ((ar(i) + ar(j))% k == 0) {
+          count += 1
+          println((i,j))
       }
     }
+  }
     count
  }
+
+val big = Array(43,95,51,55,40,86,65,81,51,20,47,50,65,53,23,78,75,75,47,73,25,27,14,8,26,58,95,28,3,23,48,69,26,3,73,52,34,7,40,33,56,98,71,29,70,71,28,12,18,49,19,25,2,18,15,41,51,42,46,19,98,56,54,98,72,25,16,49,34,99,48,93,64,44,50,91,44,17,63,27,3,65,75,19,68,30,43,37,72,54,82,92,37,52,72,62,3,88,82,71)
 }
 import Solution._
 
-println(divisibleSumPairs(6,3, Array(1, 3, 2, 6, 1, 2)))
-
-//6 3
-//1 3 2 6 1 2
+println(divisibleSumPairs(100,22, big))
+println(divisibleSumPairs(6, 3, Array(1,3,2,6,1,2)))
